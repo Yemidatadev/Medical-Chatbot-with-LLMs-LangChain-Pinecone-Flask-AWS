a4a0c5f1f7892b84e7cf77f5e842ef4a6b293af9
Yemidatadev <117512040+Yemidatadev@users.noreply.github.com>
2025-11-10 11:03:04 +0100

    complete code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Yemi Adeogun",
+    author_email="yemidatadev@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
